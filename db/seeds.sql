8679db264cbbed8e5f3fbeeebb1a015b8c70c338
kyoukel <kimyoukel@gmail.com>
2018-05-16 21:17:01 -0700

    installed mysql, added db folder, added schema.sql to create db and table and seeds.sql files to insert table data for testing

---
+++ db/seeds.sql
@@ -0,0 +1,27 @@
+INSERT INTO checklist (tent_ware) VALUES ('tent and stakes');
+INSERT INTO checklist (tent_ware) VALUES ('tools');
+INSERT INTO checklist (tent_ware) VALUES ('tarp');
+INSERT INTO checklist (tent_ware) VALUES ('blow-up mattress');
+INSERT INTO checklist (tent_ware) VALUES ('sleeping bags');
+
+INSERT INTO checklist (kitchen_ware) VALUES ('camp stove');
+INSERT INTO checklist (kitchen_ware) VALUES ('pots and pans');
+INSERT INTO checklist (kitchen_ware) VALUES ('fuel');
+INSERT INTO checklist (kitchen_ware) VALUES ('utensils');
+INSERT INTO checklist (kitchen_ware) VALUES ('coffee and device');
+
+INSERT INTO checklist (kitchen_cleanup) VALUES ('dish soap');
+INSERT INTO checklist (kitchen_cleanup) VALUES ('scrubbing sponges');
+INSERT INTO checklist (kitchen_cleanup) VALUES ('trash bags');
+INSERT INTO checklist (kitchen_cleanup) VALUES ('paper towels');
+INSERT INTO checklist (kitchen_cleanup) VALUES ('washbins');
+
+INSERT INTO checklist (campfire) VALUES ('firewood');
+INSERT INTO checklist (campfire) VALUES ('lighter');
+INSERT INTO checklist (campfire) VALUES ('marshmallows');
+INSERT INTO checklist (campfire) VALUES ('tongs');
+INSERT INTO checklist (campfire) VALUES ('graham crackers & chocolate');
+
+INSERT INTO checklist (electronics) VALUES ('phone and charger pack', true);
+INSERT INTO checklist (electronics) VALUES ('tablet');
+INSERT INTO checklist (electronics) VALUES ('car charging adapters', true);
