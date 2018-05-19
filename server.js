cba693d3b42ab94f7f36446d6de316fab6584b23
Anthony Cortez <anthony1154@AnthonyrtezsMBP.attlocal.net>
2018-05-18 21:04:07 -0700

    Update

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport')
+var keys = require('./config/keys.js');
+require('./models/user.js')
 require('./services/passport.js');
 
+mongoose.connect(keys.mongoUri);
+
 const app = express();
+
+app.use (
+    cookieSession ({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys:[keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());  
 
 require('./routes/authRoutes.js')(app);
 
